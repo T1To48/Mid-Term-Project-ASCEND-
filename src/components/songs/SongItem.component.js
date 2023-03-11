6e11d243aed49e4cdfe608e2919d3233aeedd56c
T1To48 <tofek.98@gmail.com>
2023-03-11 16:15:31 +0200

    background update,prettier for songitem

--- src/components/songs/SongItem.component.js
+++ src/components/songs/SongItem.component.js
@@ -1,39 +1,41 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import IconButton from '@mui/material/IconButton'
-import AddIcon from '@mui/icons-material/Add';
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
 import ListItem from "@mui/material/ListItem";
-const SongItem = ({songTitle,songId,onClick}) => {
-   const [isLoading,setIsLoading]=useState(true);
-   
-   useEffect(() => {
-    setTimeout(()=>{
-      setIsLoading(false)
+const SongItem = ({ songTitle, songId, onClick }) => {
+  const [isLoading, setIsLoading] = useState(true);
 
-   },1000)
-   
-   }, []);
-   
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-   <div >
-   <ListItem  disablePadding style={{  margin: "0 10%" }}>
-     <IconButton onClick={onClick} sx={{ float: "right" }} color="primary">
-       <AddIcon />
-     </IconButton>
-     {isLoading ? "Loading...":<iframe
-       title={`Song ${songTitle}`}
-       style={{ borderRadius: "12px"}}
-       src={`https://open.spotify.com/embed/track/${songId}?utm_source=generator`}
-       width="80%"
-       height="170"
-       frameBorder="0"
-       allowFullScreen=""
-       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-       loading="lazy"
-     />}
-   </ListItem>
-   </div>
-  )
-}
+    <div>
+      <ListItem disablePadding style={{ margin: "0 10%" }}>
+        <IconButton onClick={onClick} sx={{ float: "right" }} color="primary">
+          <AddIcon />
+        </IconButton>
+        {isLoading ? (
+          "Loading..."
+        ) : (
+          <iframe
+            title={`Song ${songTitle}`}
+            style={{ borderRadius: "12px" }}
+            src={`https://open.spotify.com/embed/track/${songId}?utm_source=generator`}
+            width="80%"
+            height="170"
+            frameBorder="0"
+            allowFullScreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          />
+        )}
+      </ListItem>
+    </div>
+  );
+};
 
-export default SongItem
+export default SongItem;
